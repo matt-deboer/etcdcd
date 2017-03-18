4ef1216a7e1e8284be1b466b13b5f55ea96b3434
Matt DeBoer <matt.deboer@gettyimages.com>
2017-03-17 22:51:29 -0700

    fixes for aws provider

--- pkg/platform/all/all.go
+++ pkg/platform/all/all.go
@@ -2,5 +2,6 @@ package all
 
 import (
 	// Platforms
+	_ "github.com/matt-deboer/etcdcd/pkg/platform/aws"
 	_ "github.com/matt-deboer/etcdcd/pkg/platform/vsphere"
 )
--- pkg/platform/aws/aws.go
+++ pkg/platform/aws/aws.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"io"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/matt-deboer/etcdcd/pkg/platform"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
@@ -16,24 +19,34 @@ import (
 
 func init() {
 	platform.Register("aws", func(config io.Reader) (platform.Platform, error) {
-		return &AWS{}, nil
+		return newAWS()
 	})
 }
 
 // AWS is a Platform implementation for AWS
 type AWS struct {
 	localInstanceID string
+	region          *string
+	credentials     *credentials.Credentials
 }
 
 func newAWS() (*AWS, error) {
-	sess := session.Must(session.NewSession())
-	svc := ec2metadata.New(sess)
+	svc := ec2metadata.New(session.New(&aws.Config{}))
 	doc, err := svc.GetInstanceIdentityDocument()
 	if err != nil {
 		return nil, err
 	}
 	aws := &AWS{
 		localInstanceID: doc.InstanceID,
+		region:          &doc.Region,
+		credentials: credentials.NewChainCredentials(
+			[]credentials.Provider{
+				&credentials.EnvProvider{},
+				&ec2rolecreds.EC2RoleProvider{
+					Client: ec2metadata.New(session.New(&aws.Config{})),
+				},
+				&credentials.SharedCredentialsProvider{},
+			}),
 	}
 	return aws, nil
 }
@@ -42,32 +55,47 @@ func newAWS() (*AWS, error) {
 func (a *AWS) ExpectedMembers(
 	memberFilter string, clientScheme string, clientPort int, serverScheme string, serverPort int) ([]etcd.Member, error) {
 
-	sess := session.Must(session.NewSession())
-	instanceIDs, err := getASGInstanceIDs(sess, memberFilter)
+	sess := session.New(&aws.Config{
+		Region:      a.region,
+		Credentials: a.credentials,
+	})
+	instanceIDs, err := a.getASGInstanceIDs(sess, memberFilter)
 	if err != nil {
 		return nil, err
 	}
+	if log.GetLevel() >= log.DebugLevel {
+		instances := ""
+		for _, i := range instanceIDs {
+			instances += "," + *i
+		}
+		log.Debugf("ExpectedMembers: for asg %s, found the following instances: [%s]", memberFilter, instances[1:])
+	}
 
 	svc := ec2.New(sess)
-
-	diParams := &ec2.DescribeInstancesInput{
-		Filters:     []*ec2.Filter{},
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("instance-state-name"),
+				Values: []*string{aws.String("running")},
+			},
+		},
 		InstanceIds: instanceIDs,
-		MaxResults:  aws.Int64(int64(len(instanceIDs))),
 	}
-	resp, err := svc.DescribeInstances(diParams)
+	resp, err := svc.DescribeInstances(params)
 	if err != nil {
 		return nil, err
 	}
 
 	expectedMembers := make([]etcd.Member, 0, len(resp.Reservations[0].Instances))
-	for _, i := range resp.Reservations[0].Instances {
-		member := etcd.Member{
-			Name:       *i.InstanceId,
-			PeerURLs:   []string{fmt.Sprintf("%s://%s:%d", serverScheme, *i.PrivateIpAddress, serverPort)},
-			ClientURLs: []string{fmt.Sprintf("%s://%s:%d", clientScheme, *i.PrivateIpAddress, clientPort)},
+	for _, r := range resp.Reservations {
+		for _, i := range r.Instances {
+			member := etcd.Member{
+				Name:       *i.InstanceId,
+				PeerURLs:   []string{fmt.Sprintf("%s://%s:%d", serverScheme, *i.PrivateIpAddress, serverPort)},
+				ClientURLs: []string{fmt.Sprintf("%s://%s:%d", clientScheme, *i.PrivateIpAddress, clientPort)},
+			}
+			expectedMembers = append(expectedMembers, member)
 		}
-		expectedMembers = append(expectedMembers, member)
 	}
 
 	return expectedMembers, nil
@@ -78,7 +106,7 @@ func (a *AWS) LocalInstanceName() string {
 	return a.localInstanceID
 }
 
-func getASGInstanceIDs(sess *session.Session, asgName string) ([]*string, error) {
+func (a *AWS) getASGInstanceIDs(sess *session.Session, asgName string) ([]*string, error) {
 	svc := autoscaling.New(sess)
 
 	params := &autoscaling.DescribeAutoScalingGroupsInput{
